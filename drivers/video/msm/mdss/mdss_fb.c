cc728c4da4d65312e2cff4fb78265cd29933c73a
davidevinavil <davidevinavil@gmail.com>
2017-02-06 23:25:31 +0100

    video: mdss: Revert LiveDisplay driver

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -56,8 +56,6 @@
 #include "mdss_mdp.h"
 #include "lcd_effect.h"
 
-#include "mdss_livedisplay.h"
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -1077,7 +1075,7 @@ static int mdss_fb_create_sysfs(struct msm_fb_data_type *mfd)
 	if (rc)
 		pr_err("sysfs group creation failed, rc=%d\n", rc);
 
-	return mdss_livedisplay_create_sysfs(mfd);
+	return rc;
 }
 
 static void mdss_fb_remove_sysfs(struct msm_fb_data_type *mfd)
